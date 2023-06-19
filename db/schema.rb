efba43a75fa2ceacfec6a6d90b9e6894c43434b3
Morganne Maziere <morganne.maziere@kedgebs.com>
2023-06-19 17:58:47 +0200

    seed_tent_pat

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_144905) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_152701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tents", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.boolean "availability"
+    t.string "address"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tents_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_144905) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tents", "users"
 end
