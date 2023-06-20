f4287c95b5bb5a4a4bd949b6b6ac928597ee45c1
Morganne Maziere <morganne.maziere@kedgebs.com>
2023-06-20 18:02:45 +0200

    reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_141257) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_144633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_141257) do
     t.datetime "updated_at", null: false
     t.index ["tent_id"], name: "index_bookings_on_tent_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "comment"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "tents", force: :cascade do |t|
@@ -54,5 +63,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_141257) do
 
   add_foreign_key "bookings", "tents"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bookings"
   add_foreign_key "tents", "users"
 end
