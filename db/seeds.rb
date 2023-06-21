2f0cfc4d677dd9f0603e375694571f41bd7d75d4
Anouk Semerdjian <anouk.semerdjian@kedgebs.com>
2023-06-21 14:27:47 +0200

    show

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@ Tent.destroy_all
 User.destroy_all
 
 puts "create user"
-patrick = User.create!(first_name: "Patrick", email: "patrick.amazing@gmail.com", password: "patrick")
+patrick = User.create!(first_name: "Patrick", email: "patrick.amazing@gmail.com", password: "patrick", profilepic: "https://source.unsplash.com/random/?person")
 puts "Finished user!"
 
 puts "create tent"
-tent1 = Tent.create!(name: "lakehouse", description: "Amazing view on the lake", address: "31 lake street", price: 150, user: patrick)
-tent2 = Tent.create!(name: "oceanhouse", description: "Amazing view on the ocean", address: "31 ocean street", price: 200, user: patrick)
-tent3 = Tent.create!(name: "riverhouse", description: "Amazing view on the river", address: "31 river street", price: 90, user: patrick)
+tent1 = Tent.create!(name: "lakehouse", description: "Amazing view on the lake", address: "31 lake street", price: 150, user: patrick, photo_url: "https://source.unsplash.com/random/?tent")
+tent2 = Tent.create!(name: "oceanhouse", description: "Amazing view on the ocean", address: "31 ocean street", price: 200, user: patrick, photo_url: "https://source.unsplash.com/random/?tent")
+tent3 = Tent.create!(name: "riverhouse", description: "Amazing view on the river", address: "31 river street", price: 90, user: patrick, photo_url: "https://source.unsplash.com/random/?tent")
 puts "Finished tent!"
